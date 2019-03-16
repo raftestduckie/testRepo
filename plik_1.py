9c6272c1eff3b48ad43e951837273b227218b3cf
raftestduckie <48618927+raftestduckie@users.noreply.github.com>
2019-03-16 12:38:28 +0100

    Add files via upload
    wersja nr 1

---
+++ plik_1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 16 12:07:05 2019
+
+@author: rafal
+"""
+
+a = [2, 3]
+b = 2
+print(a)
+print(a[0], b)
---
+++ plik_2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 16 12:10:24 2019
+
+@author: rafal
+"""
+
+a2 =['jhk', 'poio', 'tr']
+print(a2[2])
