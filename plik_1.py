c23cd469465b7eae723d1de5d72b41eb17aef327
rafbb100 <rafbb100@gmail.com>
2020-01-17 11:22:28 +0100

    kjkjkj

--- plik_1.py
+++ plik_1.py
@@ -5,9 +5,11 @@ Created on Sat Mar 16 12:07:05 2019
 @author: rafal
 """
 
+import plik_2
+
 a = [2, 3]
 b = 2
 print(a)
-print(a[0], b)
+print(a[0], b, 'plik_1_1")
 
 c = 'rere' # dodane 22.03 12.40 do master
